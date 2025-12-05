65639c70c668f1cbdf9a7472453aff86209c5cca
hoan99111 <hoan99111@gmail.com>
2025-12-05 17:02:19 +0700

    bai 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import studentModel from "./model/student.model.js";
+import connectDB from "./dbConnection/ConnectDb.js";
 
 const app = express();
 
@@ -12,21 +12,12 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose
-  .connect("mongodb://localhost:27017/student_db")
-  .then(() => console.log("Đã kết nối MongoDB thành công"))
-  .catch((err) => console.error("Lỗi kết nối MongoDB:", err));
 
 // Test route
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-// Start server
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
-});
 
 // API route to get all students
 app.get("/api/students", async (req, res) => {
@@ -37,3 +28,14 @@ app.get("/api/students", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+const startServer = async () => {
+  await connectDB();
+
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
+  });
+};
+
+startServer();
